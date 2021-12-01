d709f459293e6bcbdc4ef3a0f0acf61d35e180f0
Daniele Marques <danirbd13@hotmail.com>
2021-12-01 16:49:32 -0300

    criando o arquivo routes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import { Header } from "./Components/Header/Header";
-import { List } from "./Components/List/List";
-import { Navbar } from "./Components/Navbar/Navbar";
-import { Footer } from "./Components/Footer/Footer";
+import Routes from "./Routes";
 function App() {
   return (
     <>
-      <Navbar />
-      <Header />
-      <List />
-      <Footer />
+      <Routes />
     </>
   );
 }
